ee9a08ec0fac5f20b5f4933b4793880873a22e57
Marcelo Lauxen <marcelolauxen16@gmail.com>
2018-11-19 02:53:18 -0200

    Nowa.Model changes

--- Nowa.Model.pas
+++ Nowa.Model.pas
@@ -6,14 +6,22 @@ uses
   Nowa.Records;
 
 type
+  IField = interface
+  ['{B4D2955E-B979-4EE0-96AD-5B12311635C7}']
+    function Name: String;
+    function Alias: String;
+    function Table: String;
+    function TableAlias: String;
+  end;
+
   IModel<T> = interface
   ['{C466292A-8F89-4B1F-B3BF-EE5588621D00}']
     procedure PrepareModel(const ATableAlias: String; const AFields: TArray<T>);
     procedure SetValue(const AField: T; const AValue: Variant);
 
     function GetValue(const AField: T): Variant;
-    function FieldName(const AField: T): String;
-    function FieldAliasName(const AField: T): string;
+    function Field(const AField: T): IField;
+
     function GetFields: TArray<String>;
     function GetEnumeratedFields: TArray<T>;
     function GetFieldsAlias: TArray<String>;
